f10ff85235e0587ef635d6bb8ef36475a2ba566b
weikz <zhangkon@gmail.com>
2017-04-15 10:53:11 +0800

    admin page

--- src/components/AdminPage/index.js
+++ src/components/AdminPage/index.js
@@ -1,10 +1,12 @@
 import React from 'React';
+import Helmet from 'react-helmet';
 
 import Navigation from './Navigation';
 import OrderPanel from './OrderPanel';
 
 const AdminPage = () => (
     <div>
+        <Helmet title="後台管理"/>
         <Navigation />
         <OrderPanel />
     </div>
